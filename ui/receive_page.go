4831c936a5275d0bc25389f013bed5865221fee3
Collins Olanrewaju <ocollins444@gmail.com>
2021-05-31 10:10:32 +0100

    Fix rebase errors

--- ui/receive_page.go
+++ ui/receive_page.go
@@ -213,7 +213,7 @@ func (pg *receivePage) topNav(gtx layout.Context, common pageCommon) layout.Dime
 			)
 		}),
 		layout.Flexed(1, func(gtx C) D {
-			return layout.E.Layout(gtx, common.subPageInfoButton.Layout)
+			return layout.E.Layout(gtx, pg.info.Layout)
 		}),
 	)
 }
